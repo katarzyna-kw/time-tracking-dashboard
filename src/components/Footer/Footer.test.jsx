fbacf02f11cb814565bd3608f124dc1520c77399
katarzyna_w <katarzyna.koch@gmail.com>
2022-02-08 20:12:38 -0600

    Footer test

--- src/components/Footer/Footer.test.jsx
+++ src/components/Footer/Footer.test.jsx
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import Footer from './index'
-test('renders footer', () => {
+import { render, screen } from "@testing-library/react";
+import Footer from "./index";
+test("renders footer", () => {
   render(<Footer />);
   const footerElement = screen.getByTestId("footer");
   expect(footerElement).toBeInTheDocument();
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
-   return (
-      <footer className="attribution" data-testid="footer">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
-        Coded by <a href="https://katarzyna-kw.github.io/portfolio-website/" target="_blank" rel="noreferrer">Katarzyna Wegrzynowicz</a>.
-      </footer>
-   )
+  return (
+    <footer className="attribution" data-testid="footer">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        href="https://katarzyna-kw.github.io/portfolio-website/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Katarzyna Wegrzynowicz
+      </a>
+      .
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
